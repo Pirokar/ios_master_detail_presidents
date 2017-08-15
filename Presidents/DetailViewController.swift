bea996bf70c659f34ebe35bd4027c15d038a328a
Владимир Рыбалка <vrybalka@initium.ru>
2017-08-15 12:47:42 +0300

    Implemented detail webview

--- Presidents/DetailViewController.swift
+++ Presidents/DetailViewController.swift
@@ -11,13 +11,22 @@ import UIKit
 class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
 
 
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
             if let label = detailDescriptionLabel {
-                label.text = detail.description
+                let dict = detail
+                let urlString = dict["url"]!
+                label.text = urlString
+                
+                let url = NSURL(string: urlString)
+                let request = URLRequest(url: url! as URL)
+                webView.loadRequest(request)
+                let name = dict["name"]!
+                title = name
             }
         }
     }
@@ -33,7 +42,7 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    var detailItem: NSDate? {
+    var detailItem: [String: String]? {
         didSet {
             // Update the view.
             configureView()
